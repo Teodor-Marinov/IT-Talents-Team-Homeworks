3ecc10ee75689c2017ee21f6f237de76fc324810
Teodor Marinov <T_M_Marinov@yahoo.co.uk>
2015-12-13 14:22:11 +0200

    Updated Tic Tac Toe

--- Zadachki zakachki/TicTacToe/src/Program.java
+++ Zadachki zakachki/TicTacToe/src/Program.java
@@ -2,13 +2,21 @@
 public class Program {
 	public static void main(String[] args) {
 		Game game = new Game();
-		while (!game.hasWinner() || !game.isDraw()) {
+		int count = 0;
+		// while (!game.hasWinner() || !game.isDraw()) {
+		while (count < 9 && !game.hasWinner()) {
 			game.changePlayerChar();
 			game.makeMove();
-			game.printField();			
-			game.hasWinner();
-			game.isDraw();
-			
+			game.printField();
+
+			if (game.hasWinner()) {
+				break;
+			}
+			if (!game.hasWinner() && count == 8) {
+				System.out.println("This game is a draw.");
+				break;
+			}
+			count++;
 		}
 	}
 }
