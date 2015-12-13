e2ad1e0e2471c2ecafd4a642e01d857cbde1cc16
Teodor Marinov <T_M_Marinov@yahoo.co.uk>
2015-12-13 20:42:48 +0200

    Zakachka 3 update

--- Zadachki zakachki/Zakachlivka_3.java
+++ Zadachki zakachki/Zakachlivka_3.java
@@ -16,22 +16,42 @@ public class Zakachlivka_3 {
 		int n = sc.nextInt();
 		System.out.println("Enter number of columns");
 		int m = sc.nextInt();
-		int[][] field = new int[n][m];
+		char[][] field = new char[n][m];
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[0].length; j++) {
+				field[i][j] = ' ';
+			}
+		}
 		System.out.println("Enter cooridnates within the limits of the field");
 		int row = sc.nextInt() - 1;
 		int col = sc.nextInt() - 1;
 		if ((row < 0) || (row >= n) || (col < 0) || (col >= m)) {
 			System.out.println("Invalid coordinate");
 		} else {
-
-			nextMove(field, row, col);
+			if ((n >= 3 && m > 3) || (n > 3 && m >= 3)) {
+				for (int i = 0; i < field.length; i++) {
+					for (int j = 0; j < field[0].length; j++) {
+						field[i][j] = 'o';
+					}
+				}
+			} else {
+				nextMove(field, row, col);
+			}
 
 			System.out.println("Possible position of the horse, starting from position " + (row + 1) + ";" + (col + 1)
-					+ ", are marked with \"1\"");
-
+					+ ", are marked with \"o\"");
+			for (int j2 = 0; j2 < field[0].length; j2++) {
+				System.out.print("----");
+			}
+			System.out.println();
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < m; j++) {
-					System.out.print(field[i][j] + " ");
+
+					System.out.print("| " + field[i][j] + " ");
+				}
+				System.out.print("|\n");
+				for (int j2 = 0; j2 < field[0].length; j2++) {
+					System.out.print("----");
 				}
 				System.out.println();
 			}
@@ -42,11 +62,11 @@ public class Zakachlivka_3 {
 
 	// рекурсивна ф-я:
 
-	static void nextMove(int[][] field, int r, int c) {
-		if (field[r][c] != 0) {
+	static void nextMove(char[][] field, int r, int c) {
+		if (field[r][c] != ' ') {
 			return;
 		}
-		field[r][c] = 1;
+		field[r][c] = 'o';
 		if ((r - 2 >= 0) && (c - 1 >= 0)) {
 			nextMove(field, r - 2, c - 1);
 		}
